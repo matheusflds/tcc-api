6c270b6b541a05fcd61562fd70ea3a694a748186
Matheus Felix <flds.matheus@gmail.com>
2019-11-16 10:00:14 -0300

    feat: add flask_restfull in order to improve project structure

--- app.py
+++ app.py
@@ -1,40 +1,29 @@
-from flask import Flask, jsonify, request
-from topic_modelling.topic_model import TopicModel
-from flask_cors import CORS, cross_origin
+from flask import Flask
+from flask_restful import Resource, Api
+from flask_cors import CORS
+from resources.terms import Terms
 
 app = Flask(__name__)
+api = Api(app)
+api.add_resource(Terms, '/terms')
 
-cors = CORS(app)
+CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/topics", methods=['GET'])
-@cross_origin()
-def topics():
-  # query = request.args.get('query')
-  # topic_model = TopicModel('datasets/topic_modelling')
-  # topics = topic_model.get_topics(query)
-  # response = [list(map(lambda x: [x[1], x[0]], topic)) for topic in topics]
-  response = [
-    [['apple', 0.05], ['banana', 0.01], ['grape', 0.04], ['strawberry', 0.03], ['lemon', 0.05], ['orange', 0.01], ['kiwi', 0.04], ['tree', 0.03]],
-    [['car', 0.05], ['motor', 0.01], ['machine', 0.04], ['engine', 0.03], ['bicycle', 0.05]],
-  ]
-  print('done')
-  return jsonify({
-    'topics': response
-  })
-
-@app.route("/terms")
-@cross_origin()
-def terms():
-  terms = [
-    ['nobel', 0.5],
-    ['blizzard', 0.3],
-    ['trump', 0.4],
-    ['brazil', 0.1],
-  ]
-  return jsonify({
-    'terms': terms
-  })
+# @app.route("/topics", methods=['GET'])
+# def topics():
+#   # query = request.args.get('query')
+#   # topic_model = TopicModel('datasets/topic_modelling')
+#   # topics = topic_model.get_topics(query)
+#   # response = [list(map(lambda x: [x[1], x[0]], topic)) for topic in topics]
+#   response = [
+#     [['apple', 0.05], ['banana', 0.01], ['grape', 0.04], ['strawberry', 0.03], ['lemon', 0.05], ['orange', 0.01], ['kiwi', 0.04], ['tree', 0.03]],
+#     [['car', 0.05], ['motor', 0.01], ['machine', 0.04], ['engine', 0.03], ['bicycle', 0.05]],
+#   ]
+#   print('done')
+#   return jsonify({
+#     'topics': response
+#   })
 
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
---
+++ resources/terms.py
@@ -0,0 +1,17 @@
+from flask_restful import Resource
+from flask import jsonify
+
+class Terms(Resource):
+  def get(self):
+    terms = [
+      ['nobel', 0.5],
+      ['blizzard', 0.3],
+      ['trump', 0.4],
+      ['brazil', 0.1],
+    ]
+    if terms:
+      return jsonify(terms)
+    return {
+      'message': 'Terms not found',
+      'code': '404'
+    }
