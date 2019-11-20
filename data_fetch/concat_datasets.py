b62abed42300bcfa197ff70d02f3371f7e03bea3
Matheus Felix <flds.matheus@gmail.com>
2019-11-20 19:55:26 -0300

    feat: get tweets related to term

---
+++ data_fetch/concat_datasets.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+
+from pathlib import Path
+from datetime import datetime
+
+def concat_datasets(dataset_dir):
+  DATASET_DIR = Path(dataset_dir).resolve()
+  files = pd.Series([x for x in os.listdir(DATASET_DIR) if x.endswith('.csv')], 
+                     name='files')
+  if len(files) > 0:
+    queries = files.str.extract(r'([#:a-zA-Z]\w+:?)', expand=False)
+    for query in queries.unique():
+      query_files = files[files.str.contains(query)]
+      print('Found {} file(s) with query "'.format(len(query_files)) + query + '"')
+      df = pd.DataFrame(columns=['id', 'date', 'user', 'text'])
+      for filename in query_files:
+        filepath = os.path.join(DATASET_DIR, filename)
+        df = pd.concat([df, pd.read_csv(filepath)])
+        os.remove(filepath)
+
+      now = datetime.now()
+      timestamp = int(datetime.timestamp(now))
+      name = query + '-' + str(timestamp) + '.csv'
+      df.to_csv(os.path.join(DATASET_DIR, name), index=None)
+  print('Done!')
+
+if __name__ == '__main__':
+  from argparse import ArgumentParser
+
+  parser = ArgumentParser()
+  parser.add_argument('dataset_dir', type=str)
+
+  args = parser.parse_args()
+  concat_datasets(**vars(args))
--- resources/terms.py
+++ resources/terms.py
@@ -1,12 +1,16 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask import jsonify
 from datetime import datetime
 from math import log
 
 from models import TermModel
+from data_fetch.get_tweet import get_tweets
 
 class Terms(Resource):
   MAX_TIMESTAMP_DIFF = 2826090
+
+  parser = reqparse.RequestParser()
+  parser.add_argument('term_text', required=True, help='This field cannot be left empty')
 
   def get(self):
     terms = [[term.text, self._calculate_weight(term)] for term in TermModel.query.all()]
@@ -16,6 +20,20 @@ class Terms(Resource):
       'message': 'Terms not found',
       'code': '404'
     }
+
+  def post(self):
+    req_data = Terms.parser.parse_args()
+    query = req_data['term_text']
+    # term_df = get_tweets(query, save_dir='datasets', max_requests=100, count=100)
+    term_df = get_tweets(query, save_dir='datasets', max_requests=1, count=10)
+    if term_df is not None:
+      print(term_df.head())
+    else:
+      print('aaaaaa')
+    return jsonify({ 
+      'message': 'Succesfully processed tweets for specified term' 
+    })
+
 
   def _calculate_weight(self, term):
     current = datetime.timestamp(datetime.now())
