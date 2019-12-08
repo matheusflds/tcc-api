2ca4c5bb5a3b65cbc43a56ed7349516142a264b7
Matheus Felix <flds.matheus@gmail.com>
2019-12-07 22:33:44 -0300

    fix: increase topic word size

--- app/resources/topics/topic_model.py
+++ app/resources/topics/topic_model.py
@@ -8,7 +8,7 @@ class TopicDBModel(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   term_id = db.Column(db.Integer, db.ForeignKey('terms.id'))
-  words = db.Column(ARRAY(db.String(32)))
+  words = db.Column(ARRAY(db.String(64)))
   words_probability = db.Column(ARRAY(db.Integer))
   polarity = db.Column(db.Float)
   anger_percentage = db.Column(db.Float)
